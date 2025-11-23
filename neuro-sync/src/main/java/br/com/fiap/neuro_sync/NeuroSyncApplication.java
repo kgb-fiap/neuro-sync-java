6b3727fed6c441850358ae75c40919f194ab70eb
gabrielCZz <gabriel.cruz2512@gmail.com>
2025-11-23 01:03:29 -0300

    initial commit

---
+++ neuro-sync/src/main/java/br/com/fiap/neuro_sync/NeuroSyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.neuro_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroSyncApplication.class, args);
+	}
+
+}
---
+++ neuro-sync/src/test/java/br/com/fiap/neuro_sync/NeuroSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.neuro_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
